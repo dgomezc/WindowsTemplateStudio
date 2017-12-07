6bec2717d2840b11f0da7e12f8c0a970a4b913a5
David Gómez <v-davgom@microsoft.com>
2017-12-07 12:28:54 +0100

    Add create and update new item

--- poc/EasyTablesPoc/EasyTablesPoc/ViewModels/MainViewModel.cs
+++ poc/EasyTablesPoc/EasyTablesPoc/ViewModels/MainViewModel.cs
@@ -9,7 +9,14 @@ using System.Linq;
 namespace EasyTablesPoc.ViewModels
 {
     public class MainViewModel : Observable
-    {        
+    {
+        private string _statusText;
+        public string StatusText
+        {
+            get => _statusText;
+            set => Set(ref _statusText, value);
+        }
+
         private bool _isBusy;
         public bool IsBusy
         {
@@ -17,9 +24,10 @@ namespace EasyTablesPoc.ViewModels
             set
             {
                 Set(ref _isBusy, value);
-                AddItemCommand.OnCanExecuteChanged();
-                DeleteItemCommand.OnCanExecuteChanged();
                 LoadItemsCommand.OnCanExecuteChanged();
+                NewItemCommand.OnCanExecuteChanged();
+                SaveItemCommand.OnCanExecuteChanged();
+                DeleteItemCommand.OnCanExecuteChanged();
             }
         }
 
@@ -29,72 +37,112 @@ namespace EasyTablesPoc.ViewModels
             get => _items;
             set => Set(ref _items, value);
         }
-
+        
         private Food _selectedItem;
         public Food SelectedItem
         {
             get => _selectedItem;
-            set 
+            set
             {
                 Set(ref _selectedItem, value);
+                if(value != null)
+                {
+                    EditableItem = value;
+                }
+            }
+        }
+
+        private Food _editableItem;
+        public Food EditableItem
+        {
+            get => _editableItem;
+            set
+            {
+                Set(ref _editableItem, value);
                 DeleteItemCommand.OnCanExecuteChanged();
             }
         }
 
         private RelayCommand _loadItemsCommand;
-        public RelayCommand LoadItemsCommand => _loadItemsCommand ?? (_loadItemsCommand = new RelayCommand(async () => await LoadItems(), () => !IsBusy));
+        public RelayCommand LoadItemsCommand => _loadItemsCommand ?? (_loadItemsCommand = new RelayCommand(async () => await LoadItemsAsync(), () => !IsBusy));
 
-        private RelayCommand _addItemCommand;
-        public RelayCommand AddItemCommand => _addItemCommand ?? (_addItemCommand = new RelayCommand(async () => await AddItem(), () => !IsBusy));
+        private RelayCommand _newItemCommand;
+        public RelayCommand NewItemCommand => _newItemCommand ?? (_newItemCommand = new RelayCommand(CreateEmptyItem, () => !IsBusy));
+
+        private RelayCommand _saveItemCommand;
+        public RelayCommand SaveItemCommand => _saveItemCommand ?? (_saveItemCommand = new RelayCommand(async () => await SaveItem(), () => !IsBusy));
 
         private RelayCommand _deleteItemCommand;
-        public RelayCommand DeleteItemCommand => _deleteItemCommand ?? (_deleteItemCommand = new RelayCommand(async () => await DeleteItem(SelectedItem), () => SelectedItem != null && !IsBusy));
+        public RelayCommand DeleteItemCommand => _deleteItemCommand ?? (_deleteItemCommand = new RelayCommand(async () => await DeleteItem(), () =>!IsBusy && CanDeleteItem()));
         
         private readonly FoodService _service = FoodService.Instance;
 
         public MainViewModel()
         {
+            CreateEmptyItem();
         }
 
-        private async Task LoadItems()
+        private async Task RefreshItems()
+        {
+            StatusText = "Loading items from Azure Easy Tables...";
+
+            var items = await _service.ReadAsync();
+            Items = new ObservableCollection<Food>(items);
+
+            SelectedItem = Items.FirstOrDefault(i => i.Id == EditableItem?.Id);            
+        }
+
+        private async Task LoadItemsAsync()
         {
             if (!IsBusy)
             {
                 IsBusy = true;
-
-                var items = await _service.ReadAsync();
-                Items = new ObservableCollection<Food>(items);
-
+                await RefreshItems();
                 IsBusy = false;
+
+                StatusText = "Finished load items!.";
             }
         }
 
-        private async Task AddItem()
+        private void CreateEmptyItem()
+        {
+            EditableItem = new Food ();
+        }
+
+        private async Task SaveItem()
         {
             if (!IsBusy)
             {
                 IsBusy = true;
+                StatusText = "Save item...";
 
-                await _service.AddOrUpdateAsync(new Food { Name = "Example Name", Category = "Example Category" });
+                await _service.AddOrUpdateAsync(EditableItem);
+                await RefreshItems();
 
                 IsBusy = false;
-
-                await LoadItems();
+                StatusText = "Item saved completed!.";
             }
         }
 
-        private async Task DeleteItem(Food item)
+        private async Task DeleteItem()
         {
             if (!IsBusy)
             {
                 IsBusy = true;
+                StatusText = "Remove item...";
 
-                await _service.DeleteAsync(item);
+                await _service.DeleteAsync(EditableItem);
+                EditableItem = new Food();
+                await RefreshItems();
 
                 IsBusy = false;
-
-                await LoadItems();
+                StatusText = "Remove item completed!.";
             }
+        }
+
+        private bool CanDeleteItem()
+        {
+            return !string.IsNullOrEmpty(EditableItem?.Id);
         }
     }
 }
